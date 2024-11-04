79b4f11de7ab0ee93dedae5f3dfc7cb4a8223c95
dshpedro <94130906+dshpedro@users.noreply.github.com>
2024-11-04 19:36:00 -0300

    Add subprecess to run power.py from main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import subprocess
 
 def main():
-    arcs_file = 'arcs.txt'
+    arcs_file = 'polarized_arcs.txt'
     subprocess.run(['python', 'delay.py', arcs_file])
+    subprocess.run(['python', 'power.py', arcs_file])
 
 if __name__ == '__main__':
     main()
